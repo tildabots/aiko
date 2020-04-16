f1ba714c0a37daaa2760861029da7ca1f15eb675
S Stewart <tilda@users.noreply.github.com>
2020-04-16 05:11:22 -0500

    first lookup but fixed

--- ext/profile.py
+++ ext/profile.py
@@ -7,17 +7,18 @@ from osuapi.enums import OsuMode
 class Profile(Cog):
     def __init__(self, bot):
         # everything's taken care of here
-        pass
+        super().__init__(bot)
 
     @commands.command()
     async def osu(self, ctx, user: discord.Member, mode: OsuMode = 0):
-        async with ctx.bot.kv.get(user.id) as key:
-            if key is None:
-                return await ctx.send(
-                    "I do not have you in the database."
-                    + f' Use `{self.bot.config["prefix"]}profile`'
-                    + " to begin setup."
-                )
+        print(dir(self))
+        key = await self.bot.kv.get(user.id)
+        if key is None:
+            return await ctx.send(
+                "I do not have you in the database."
+                + f' Use `{self.bot.config["prefix"]}profile`'
+                + " to begin setup."
+            )
 
 
 def setup(bot):
